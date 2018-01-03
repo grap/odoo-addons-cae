a1c9011e371958e8587c997da1b95d9fcbbe51b9
Sylvain LE GAL <sylvain.legal@grap.coop>
2018-01-03 23:56:53 +0100

    wip

--- base_fiscal_company/models/res_company.py
+++ base_fiscal_company/models/res_company.py
@@ -6,14 +6,11 @@
 
 from lxml import etree
 
-from openerp import SUPERUSER_ID
-from openerp.osv import fields
-from openerp.osv.orm import Model
-
-_DEFAULT_COMPANY_CODE = 'ZZZ'
+from odoo import api, fields, models
 
 
-class ResCompany(Model):
+
+class ResCompany(models.Model):
     _inherit = 'res.company'
 
     _RES_COMPANY_FISCAL_TYPE = [
@@ -22,105 +19,97 @@ class ResCompany(Model):
         ('fiscal_child', 'Fiscal Child Company'),
     ]
 
-    # Private function section
-    def _propagate_access_right(self, cr, uid, ids, context=None):
-        args = [('id', 'in', ids), ('fiscal_type', '=', 'fiscal_child')]
-        rc_ids = self.search(cr, uid, args, context=context)
-        for rc in self.browse(cr, uid, rc_ids, context=context):
-            ru_ids = [ru.id for ru in rc.fiscal_company.user_ids]
-            ru_new_ids = list(set(ru_ids) - set(rc.user_ids))
-            self.write(cr, uid, [rc.id], {
-                'user_ids': [(4, id) for id in list(set(ru_new_ids))]},
-                context=context)
-
     # Columns Section
-    _columns = {
-        'administrative_department_email': fields.char(
-            string='Administrative Department Email'),
-        'fiscal_type': fields.selection(
-            _RES_COMPANY_FISCAL_TYPE,
-            string='Fiscal Type', required=True),
-        'fiscal_company': fields.many2one(
-            'res.company', 'Fiscal Company'),
-        'fiscal_childs': fields.one2many(
-            'res.company', 'fiscal_company', string='Fiscal Childs',
-            readonly=True),
-        'code': fields.char(
-            string='Code', size=3,
-            help="""This field is used as a prefix to generate automatic and"""
-            """ unique reference for items (product, ...).\n"""
-            """Warning, changing this value will change the reference of all"""
-            """ items of this company.""",
-        ),
-    }
+    fiscal_type = fields.Selection(
+        selection=_RES_COMPANY_FISCAL_TYPE, string='Fiscal Type',
+        required=True, default='normal')
 
-    _defaults = {
-        'code': _DEFAULT_COMPANY_CODE,
-        'fiscal_type': 'normal',
-    }
+    fiscal_company = fields.Many2one(
+        comodel_name='res.company', string='Fiscal Company')
 
-    # Constraint Section
-    def _check_non_fiscal_child_company(self, cr, uid, ids, context=None):
-        for rc in self.browse(cr, uid, ids, context=context):
-            # skip special case of creation
-            if rc.fiscal_company:
-                if (rc.fiscal_type in ('normal', 'fiscal_mother') and
-                        rc.id != rc.fiscal_company.id):
-                    return False
-        return True
+    fiscal_childs = fields.One2many(
+        comodel_name='res.company', inverse_name='fiscal_company',
+        string='Fiscal Childs', readonly=True)
 
-    def _check_fiscal_mother_company(self, cr, uid, ids, context=None):
-        for rc in self.browse(cr, uid, ids, context=context):
-            # skip special case of creation
-            if rc.fiscal_company is not None:
-                if (rc.fiscal_type == 'fiscal_child' and
-                        rc.fiscal_company.fiscal_type != 'fiscal_mother'):
-                    return False
-        return True
+    code = fields.Char(
+        string='Code', size=3,
+        help="""This field is used as a prefix to generate automatic and"""
+        """ unique reference for items (product, ...).\n"""
+        """Warning, changing this value will change the reference of all"""
+        """ items of this company.""",
+    )
 
-    _constraints = [
-        (_check_non_fiscal_child_company,
-            "You can't select an other company for a Non Fiscal Child Company",
-            ['fiscal_company', 'fiscal_type']),
-        (_check_fiscal_mother_company,
-            "Please select a Fiscal Mother Company for a Fiscal Child Company",
-            ['fiscal_company', 'fiscal_type']),
-    ]
+#    # Constraint Section
+#    @api.constrains('fiscal_company', 'fiscal_type')
+#    def _check_non_fiscal_child_company(self, cr, uid, ids, context=None):
+#        for rc in self.browse(cr, uid, ids, context=context):
+#            # skip special case of creation
+#            if rc.fiscal_company:
+#                if (rc.fiscal_type in ('normal', 'fiscal_mother') and
+#                        rc.id != rc.fiscal_company.id):
+#                    return False
+#        return True
+
+#    @api.constrains('fiscal_company', 'fiscal_type')
+#    def _check_fiscal_mother_company(self, cr, uid, ids, context=None):
+#        for rc in self.browse(cr, uid, ids, context=context):
+#            # skip special case of creation
+#            if rc.fiscal_company is not None:
+#                if (rc.fiscal_type == 'fiscal_child' and
+#                        rc.fiscal_company.fiscal_type != 'fiscal_mother'):
+#                    return False
+#        return True
+
+#    _constraints = [
+#        (_check_non_fiscal_child_company,
+#            "You can't select an other company for a Non Fiscal Child Company",
+#            []),
+#        (_check_fiscal_mother_company,
+#            "Please select a Fiscal Mother Company for a Fiscal Child Company",
+#            []),
+#    ]
+
+#    # Overload Section
+#    def fields_view_get(
+#            self, cr, uid, view_id=None, view_type='form', context=None,
+#            toolbar=False):
+#        """Add a required modifiers on the field code"""
+#        res = super(ResCompany, self).fields_view_get(
+#            cr, uid, view_id=view_id, view_type=view_type, context=context,
+#            toolbar=toolbar)
+#        if view_type in ('form', 'tree')\
+#                and 'code' in res['fields']:
+#            res['fields']['code']['required'] = True
+#            doc = etree.XML(res['arch'])
+#            node = doc.xpath("//field[@name='code']")[0]
+#            node.set('modifiers', '{"required": true}')
+#            res['arch'] = etree.tostring(doc)
+#        return res
 
     # Overload Section
-    def fields_view_get(
-            self, cr, uid, view_id=None, view_type='form', context=None,
-            toolbar=False):
-        """Add a required modifiers on the field code"""
-        res = super(ResCompany, self).fields_view_get(
-            cr, uid, view_id=view_id, view_type=view_type, context=context,
-            toolbar=toolbar)
-        if view_type in ('form', 'tree')\
-                and 'code' in res['fields']:
-            res['fields']['code']['required'] = True
-            doc = etree.XML(res['arch'])
-            node = doc.xpath("//field[@name='code']")[0]
-            node.set('modifiers', '{"required": true}')
-            res['arch'] = etree.tostring(doc)
-        return res
-
-    def create(self, cr, uid, vals, context=None):
-        # TODO: FIXME when trunk will be fixed.
-        # For the time being, it's not possible to create a company if
-        # the user is not SUPERUSER_ID
-        res = super(ResCompany, self).create(
-            cr, SUPERUSER_ID, vals, context=context)
+    @api.model
+    def create(self, vals):
+        company = super(ResCompany, self).create(vals)
         if not vals.get('fiscal_company', False):
-            self.write(cr, uid, [res], {
-                'fiscal_company': res}, context=context)
+            company.fiscal_company = company.id
         elif vals.get('fiscal_type', False) == 'fiscal_child':
-            self._propagate_access_right(
-                cr, uid, [res], context=context)
+            company._propagate_access_right()
         return res
 
-    def write(self, cr, uid, ids, vals, context=None):
-        res = super(ResCompany, self).write(
-            cr, uid, ids, vals, context=context)
+    @api.multi
+    def write(self, vals):
+        res = super(ResCompany, self).write(vals)
         if vals.get('fiscal_company', False):
-            self._propagate_access_right(cr, uid, ids, context=context)
+            self._propagate_access_right()
         return res
+
+    # Private section
+    @api.multi
+    def _propagate_access_right(self):
+        args = [('id', 'in', self.ids), ('fiscal_type', '=', 'fiscal_child')]
+        companies = self.search(args)
+        for company in companies:
+            user_ids = company.fiscal_company.user_ids.ids
+            new_user_ids = list(set(user_ids) - set(company.user_ids))
+            company.write({
+                'user_ids': [(4, id) for id in list(set(new_user_ids))]})
--- base_fiscal_company/models/res_users.py
+++ base_fiscal_company/models/res_users.py
@@ -4,39 +4,41 @@
 # @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp.osv.orm import Model
+from odoo import api, models
 
 
-class ResUsers(Model):
+class ResUsers(models.Model):
     _inherit = 'res.users'
 
+    # Overload Section
+    @api.model
+    def create(self, vals):
+        user = super(ResUsers, self).create(vals)
+        if vals.get('company_ids', False):
+            user._propagate_access_right()
+        return user
+
+    @api.multi
+    def write(self, vals):
+        res = super(ResUsers, self).write(vals)
+        if vals.get('company_ids', False):
+            self._propagate_access_right()
+        return res
+
     # Private function section
-    def _propagate_access_right(self, cr, uid, ids, context=None):
+    @api.multi
+    def _propagate_access_right(self):
         """If a user has access to a fiscal_mother company, so he'll have
         access to all the child_company"""
-        rc_new_ids = []
-        for ru in self.browse(cr, uid, ids, context=context):
-            for rc in ru.company_ids:
+        new_company_ids = []
+        for user in self:
+            for rc in user.company_ids:
                 if rc.fiscal_type == 'fiscal_mother':
                     rc_all_ids = [
                         x.id for x in rc.fiscal_company.fiscal_childs]
                     rc_existing_ids = [
-                        x.id for x in ru.company_ids]
-                    rc_new_ids += (
+                        x.id for x in user.company_ids]
+                    new_company_ids += (
                         list(set(rc_all_ids) - set(rc_existing_ids)))
-            super(ResUsers, self).write(cr, uid, [ru.id], {
-                'company_ids': [(4, id) for id in list(set(rc_new_ids))]},
-                context=context)
-
-    # Overload Section
-    def create(self, cr, uid, vals, context=None):
-        user_id = super(ResUsers, self).create(cr, uid, vals, context=context)
-        if vals.get('company_ids', False):
-            self._propagate_access_right(cr, uid, [user_id], context=context)
-        return user_id
-
-    def write(self, cr, uid, ids, vals, context=None):
-        res = super(ResUsers, self).write(cr, uid, ids, vals, context=context)
-        if vals.get('company_ids', False):
-            self._propagate_access_right(cr, uid, ids, context=context)
-        return res
+            super(ResUsers, user).write({
+                'company_ids': [(4, id) for id in list(set(new_company_ids))]})
