9f6238752813a0cd8e52053efdd2e8bcea2ef542
Sylvain LE GAL <sylvain.legal@grap.coop>
2018-03-16 11:00:39 +0100

    [REF] remove untested import

--- account_fiscal_company/models/__init__.py
+++ account_fiscal_company/models/__init__.py
@@ -1,15 +1,20 @@
 # coding: utf-8
-from . import fiscal_property_propagate_mixin
+
 #from . import account_account
 #from . import account_automatic_reconcile
 #from . import account_bank_statement
 #from . import account_invoice
-from . import account_journal
-from . import account_move
-from . import account_move_line
+#from . import account_journal
+#from . import account_move
+#from . import account_move_line
+
+# Propagation properties part
+from . import fiscal_property_propagate_mixin
 from . import product_category
 from . import product_template
 from . import res_company
-from . import res_company_create_wizard
 from . import res_partner
+
+# Wizard part
+from . import res_company_create_wizard
 #from . import res_company_create_wizard_category
