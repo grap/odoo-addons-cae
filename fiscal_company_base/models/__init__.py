532f33e059ecc24fd31015278f1a65d2fcd7aa65
Sylvain LE GAL <sylvain.legal@grap.coop>
2018-08-06 13:27:04 +0200

    [REF] move fiscal_mother_check_mixin from account to base module

--- fiscal_company_base/models/__init__.py
+++ fiscal_company_base/models/__init__.py
@@ -1,3 +1,4 @@
 # coding: utf-8
+from . import fiscal_mother_check_mixin
 from . import res_company
 from . import res_users
---
+++ fiscal_company_base/models/fiscal_mother_check_mixin.py
@@ -0,0 +1,25 @@
+# coding: utf-8
+# Copyright (C) 2018 - Today: GRAP (http://www.grap.coop)
+# @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+
+from odoo import _, api, models
+from openerp.exceptions import Warning as UserError
+
+
+class FiscalMotherCheckMixin(models.AbstractModel):
+    """ This abstract block the possibility for a model to have
+    items linked to a fiscal mother company.
+    Note that you can only inherit from this abstract, if the current model
+    has ```company_id``` and ```name``` fields defined.
+    """
+    _name = 'fiscal.mother.check.mixin'
+
+    @api.constrains('company_id')
+    def _check_fiscal_mother_company_id(self):
+        for item in self:
+            if item.company_id.fiscal_type == 'fiscal_mother':
+                raise UserError(_(
+                    "You can't affect the item %s (model '%s') to a fiscal"
+                    " mother company.") % (item.name, self._name))
