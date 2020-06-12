faac03f584dd7ba4f820fabe5efe42ee68a47706
Sylvain LE GAL <sylvain.legal@grap.coop>
2020-06-12 19:34:32 +0200

    [ADD] 12.0-ADD-fiscal_company_point_of_sale

---
+++ fiscal_company_point_of_sale/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright (C) 2014-Today: GRAP (http://www.grap.coop)
+# @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'CAE - Point Of Sale',
+    'version': '12.0.1.0.0',
+    'category': 'CAE',
+    'summary': 'Glue Module between CAE and Point of Sale modules',
+    'author': 'GRAP',
+    'website': 'http://www.grap.coop',
+    'license': 'AGPL-3',
+    'depends': [
+        'fiscal_company_base',
+        'point_of_sale',
+    ],
+    'installable': True,
+    'auto_install': True,
+}
---
+++ fiscal_company_point_of_sale/models/pos_config.py
@@ -0,0 +1,17 @@
+# Copyright (C) 2020-Today: GRAP (http://www.grap.coop)
+# @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import api, models
+
+
+class PosConfig(models.Model):
+    _inherit = 'pos.config'
+
+    @api.constrains('company_id', 'invoice_journal_id')
+    def _check_company_invoice_journal(self):
+        return True
+
+    @api.constrains('company_id', 'journal_id')
+    def _check_company_journal(self):
+        return True
