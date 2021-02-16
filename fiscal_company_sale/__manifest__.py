b3fcfcfdce9d9519a7edec6628f806a14fc3e3f1
Sylvain LE GAL <sylvain.legal@grap.coop>
2021-02-16 20:03:24 +0100

    [BLACK]

--- fiscal_company_sale/__manifest__.py
+++ fiscal_company_sale/__manifest__.py
@@ -3,20 +3,15 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
-    'name': 'CAE - Sale',
-    'version': '12.0.1.0.0',
-    'category': 'CAE',
-    'summary': 'Glue Module between CAE and Sale modules',
-    'author': 'GRAP',
-    'website': 'http://www.grap.coop',
-    'license': 'AGPL-3',
-    'depends': [
-        'fiscal_company_base',
-        'sale',
-    ],
-    'demo': [
-        'demo/product_product.xml',
-    ],
-    'installable': True,
-    'auto_install': True,
+    "name": "CAE - Sale",
+    "version": "12.0.1.0.0",
+    "category": "CAE",
+    "summary": "Glue Module between CAE and Sale modules",
+    "author": "GRAP",
+    "website": "http://www.grap.coop",
+    "license": "AGPL-3",
+    "depends": ["fiscal_company_base", "sale"],
+    "demo": ["demo/product_product.xml"],
+    "installable": True,
+    "auto_install": True,
 }
