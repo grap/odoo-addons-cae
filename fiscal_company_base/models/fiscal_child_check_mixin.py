9764a864d30b89787b2ad3c8b2fd798e8e8200e6
Sylvain LE GAL <sylvain.legal@grap.coop>
2021-07-19 22:32:06 +0200

    [ADD] new mixin to prevent link to fiscal child and use it for account.account and account.tax.

--- fiscal_company_base/models/fiscal_child_check_mixin.py
+++ fiscal_company_base/models/fiscal_child_check_mixin.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2018 - Today: GRAP (http://www.grap.coop)
+# Copyright (C) 2021 - Today: GRAP (http://www.grap.coop)
 # @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
@@ -7,24 +7,24 @@ from odoo import _, api, models
 from odoo.exceptions import ValidationError
 
 
-class FiscalMotherCheckMixin(models.AbstractModel):
+class FiscalChildCheckMixin(models.AbstractModel):
     """This abstract block the possibility for a model to have
-    items linked to a fiscal mother company.
+    items linked to a fiscal child company.
     Note that you can only inherit from this abstract, if the current model
     has ```company_id``` and ```name``` fields defined.
     """
 
-    _name = "fiscal.mother.check.mixin"
-    _description = "Fiscal Mother Check Mixin"
+    _name = "fiscal.child.check.mixin"
+    _description = "Fiscal Child Check Mixin"
 
     @api.constrains("company_id")
     def _check_fiscal_child_company_id(self):
-        bad_items = self.filtered(lambda x: x.company_id.fiscal_type == "fiscal_mother")
+        bad_items = self.filtered(lambda x: x.company_id.fiscal_type == "fiscal_child")
         if bad_items:
             raise ValidationError(
                 _(
                     "You can't affect the item(s) %s to a Fiscal"
-                    " Mother company.\n\n"
+                    " Child company.\n\n"
                     " (model '%s')"
                 )
                 % (",".join([x.name for x in bad_items]), self._name)
