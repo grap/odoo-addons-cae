977742cad621a094ac232de486d6814da0ba4c5a
Sylvain LE GAL <sylvain.legal@grap.coop>
2022-01-19 20:11:01 +0100

    [REF] move recurring_consignment_fiscal_company from grap/grap-odoo-business into grap/odoo-addons-cae

---
+++ recurring_consignment_fiscal_company/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright (C) 2014 - Today: GRAP (http://www.grap.coop)
+# @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Recurring Consignment - Fiscal Company",
+    "version": "12.0.1.1.1",
+    "summary": "Glue module for Recurring Consignment" " and fiscal company modules",
+    "category": "Sale",
+    "author": "GRAP",
+    "website": "https://github.com/grap/odoo-addons-cae",
+    "license": "AGPL-3",
+    "depends": [
+        "recurring_consignment",
+        "fiscal_company_base",
+    ],
+    "installable": True,
+    "auto_install": True,
+}
