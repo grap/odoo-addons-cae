ceab8bab01904549178802f883f3b2a0ba41183b
Sylvain LE GAL <sylvain.legal@grap.coop>
2024-11-15 00:59:40 +0100

    [ADD] fiscal_company_account_fiscal_year

---
+++ fiscal_company_account_fiscal_year/models/account_fiscal_year.py
@@ -0,0 +1,17 @@
+# Copyright (C) 2024-Today: GRAP (http://www.grap.coop)
+# @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import models
+
+
+class AccountFiscalYear(models.Model):
+    _name = "account.fiscal.year"
+    _inherit = [
+        "account.fiscal.year",
+        "fiscal.company.change.search.domain.mixin",
+        "fiscal.company.change.filtered.mixin",
+        "fiscal.company.check.company.mixin",
+    ]
+
+    _fiscal_company_forbid_fiscal_type = ["fiscal_child", "group"]
