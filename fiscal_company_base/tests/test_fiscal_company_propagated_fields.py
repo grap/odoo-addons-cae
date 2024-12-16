67581c6f4495eca244e6b7e4fb6c90c0901b7055
Sylvain LE GAL <sylvain.legal@grap.coop>
2024-12-16 22:01:40 +0100

    [IMP] fiscal_company_base: propagate configuration. VAT number

---
+++ fiscal_company_base/tests/test_fiscal_company_propagated_fields.py
@@ -0,0 +1,23 @@
+# Copyright (C) 2024 - Today: GRAP (http://www.grap.coop)
+# @author Sylvain LE GAL (https://twitter.com/legalsylvain)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from .test_abstract import TestAbstract
+
+
+class TestFiscalCompanyPropagatedFields(TestAbstract):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.correct_vat = "FR23334175221"
+
+    def test_propagated_fields_group(self):
+        self.group_company.vat = self.correct_vat
+        self.assertEqual(self.mother_company.vat, False)
+        self.assertEqual(self.child_company.vat, False)
+
+    def test_propagated_fields_fiscal_mother(self):
+        self.mother_company.vat = self.correct_vat
+        self.assertEqual(self.group_company.vat, False)
+        self.assertEqual(self.mother_company.vat, self.correct_vat)
+        self.assertEqual(self.child_company.vat, self.correct_vat)
