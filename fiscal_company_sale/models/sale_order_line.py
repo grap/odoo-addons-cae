e2081347b3209d90faeb23a3c88f29fb65f68fc0
Sylvain LE GAL <sylvain.legal@grap.coop>
2025-01-21 23:50:46 +0100

    [FIX] fiscal_company_sale/account: Full sale workflow. avoid error regarding taxes

---
+++ fiscal_company_sale/models/sale_order_line.py
@@ -0,0 +1,11 @@
+# Copyright (C) 2025-Today: GRAP (http://www.grap.coop)
+# @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import fields, models
+
+
+class SaleOrderLine(models.Model):
+    _inherit = "sale.order.line"
+
+    tax_id = fields.Many2many(check_company=False)
