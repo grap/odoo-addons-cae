2d339457c57d1795f587cc571f252029dee3fcb7
Sylvain LE GAL <sylvain.legal@grap.coop>
2025-02-22 12:57:49 +0100

    [IMP] fiscal_company_point_of_sale: prevent creation of pos.payment.method at Group or CAE level

---
+++ fiscal_company_point_of_sale/models/pos_payment_method.py
@@ -0,0 +1,15 @@
+# Copyright (C) 2025-Today: GRAP (http://www.grap.coop)
+# @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import models
+
+
+class PosPaymentMethod(models.Model):
+    _name = "pos.payment.method"
+    _inherit = [
+        "pos.payment.method",
+        "fiscal.company.check.company.mixin",
+    ]
+
+    _fiscal_company_forbid_fiscal_type = ["group", "fiscal_mother"]
