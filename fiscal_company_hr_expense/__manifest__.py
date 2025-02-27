4453854774de69ff7eda53bce9b53a6141ac937d
Sylvain LE GAL <sylvain.legal@grap.coop>
2025-02-27 18:24:31 +0100

    [ADD] fiscal_company_hr_expense

---
+++ fiscal_company_hr_expense/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright (C) 2025-Today: GRAP (http://www.grap.coop)
+# @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "CAE - Human Resources Expense",
+    "version": "16.0.1.0.0",
+    "category": "CAE",
+    "summary": "Glue Module between CAE and Human Resources Expense module",
+    "author": "GRAP",
+    "website": "https://github.com/grap/odoo-addons-cae",
+    "license": "AGPL-3",
+    "depends": ["fiscal_company_base", "hr_expense"],
+    "installable": True,
+    "auto_install": True,
+}
---
+++ fiscal_company_hr_expense/readme/DESCRIPTION.rst
@@ -0,0 +1,9 @@
+This module extend Odoo functionnalities, regarding companies features to
+manage CAE (Coopearatives of Activities and Employment) that is a special
+status for french companies.
+
+This module is a glue module for the Odoo Human Resources Expense module.
+
+**Features**
+
+* Allow to create expense sheet with accounting journals different of the current company.
