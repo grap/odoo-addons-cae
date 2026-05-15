267557b67c65312a5a4d7917d42cfedcd5ee7374
Sylvain LE GAL <sylvain.legal@grap.coop>
2026-05-15 23:39:44 +0200

    [FIX] test

--- fiscal_company_account/tests/test_mixin_change_filtered.py
+++ fiscal_company_account/tests/test_mixin_change_filtered.py
@@ -2,9 +2,12 @@
 # @author Sylvain LE GAL (https://twitter.com/legalsylvain)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
+from odoo.tests import tagged
+
 from .test_abstract import TestAbstract
 
 
+@tagged("post_install", "-at_install")
 class TestMixinChangeFiltered(TestAbstract):
     @classmethod
     def setUpClass(cls):
