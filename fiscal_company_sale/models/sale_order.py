619a6f23b8165d7a1c760635860354e19fda6b71
Quentin Dupont <quentin.dupont@grap.coop>
2026-06-22 19:09:21 +0200

    [16.0][FIX] fermente_sale-pos_order_to_create_order

--- fiscal_company_sale/models/sale_order.py
+++ fiscal_company_sale/models/sale_order.py
@@ -12,3 +12,18 @@ class SaleOrder(models.Model):
     _fiscal_company_forbid_fiscal_type = ["group", "fiscal_mother"]
 
     payment_term_id = fields.Many2one(check_company=False)
+
+    # For pos_order_to_sale_order module to avoid AccessError
+    # during _recompute_taxes launch with sale order creation
+    #
+    # In fact, _compute_tax_id (sale.order.line) use fiscal_company.
+    # Without this code, allowed_company_ids is recalculated in
+    # odoo/api.py def company(self) function with fiscal_company +
+    # user_company_ids → these can't be different.
+    def _recompute_taxes(self):
+        ctx = dict(self.env.context)
+
+        if ctx.get("pos_order_lines_data"):
+            ctx["fiscal_company_disable_switch_company"] = True
+
+        return super(SaleOrder, self.with_context(**ctx))._recompute_taxes()
